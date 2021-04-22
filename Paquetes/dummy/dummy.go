331dac2b5b1ec649e3725189bb7a7ad6e7552ea6
Facu Mediotte <facumediotte@gmail.com>
2021-04-22 18:54:07 -0300

    Creando paquetes

---
+++ Paquetes/dummy/dummy.go
@@ -0,0 +1,13 @@
+package dummy
+
+var hola_mundo string
+
+func init() {
+	hola_mundo = "Hola :D"
+}
+
+func HolaMundo() string {
+	return hola_mundo
+}
+
+//Primer letra en mayuscula significa public
